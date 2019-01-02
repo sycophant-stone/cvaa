f57f0aedcc824d953f74e61404a94464f3241f7f
kawayi <kawayi_rendroid@163.com>
2019-01-02 08:13:08 +0000

    [Mat] print rows cols

--- demo/mat.c
+++ demo/mat.c
@@ -5,9 +5,14 @@ using namespace cv;
 
 #define CVAA_LOGI (printf("%s(%d)-<%s>: ",__FILE__, __LINE__, __FUNCTION__), printf)
 
+static int test_mat_create()
+{
+    Mat m = Mat(2,3,CV_32FC(1));
+    CVAA_LOGI("h:%d,w:%d\n",m.rows,m.cols);
+}
 
 int mat_entry()
 {
-    Mat m = Mat(2,3,CV_32FC(1));
+    test_mat_create();
     return 0;
 }
